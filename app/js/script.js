2108ed71413626797d5d698f4b0cef601b38e35b
Pankaj Barnwal <suno.pankaj@gmail.com>
2022-04-30 16:16:26 +0530

    feat/filtering images from buttons in javascript

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,19 @@
 console.log('test');
+let figures = document.querySelectorAll('.section-column-photo');
+let images = document.querySelectorAll('.section-column-photo img');
+
+function filterSelection(button) {
+  if(button == "all") {
+    for(let i=0; i<images.length; i++) {
+      figures[i].classList.add('show');
+    }  
+  } else{
+    for(let i=0; i<images.length; i++) {
+      figures[i].classList.remove('show');
+      if(images[i]['alt'].indexOf(button) != -1) {
+        console.log()
+        figures[i].classList.add('show');
+      }
+    }
+  }
+}
